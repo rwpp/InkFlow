e0d41cf5d1090005dad56bf315cd5e3393cd3a34
zhangzhen01 <zhangzhen@songshuai.com>
2026-01-26 16:52:28 +0800

    add log

--- ioc/log.go
+++ ioc/log.go
@@ -1,12 +1,14 @@
 package ioc
 
 import (
+	"fmt"
 	"github.com/KNICEX/InkFlow/pkg/logx"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gopkg.in/natefinch/lumberjack.v2"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 //	func InitLogger() logx.Logger {
@@ -30,6 +32,17 @@ func InitLogger() logx.Logger {
 	if err := viper.UnmarshalKey("log", &config); err != nil {
 		panic(err)
 	}
+
+	// 确保日志文件所在目录存在
+	if config.Filename != "" {
+		logDir := filepath.Dir(config.Filename)
+		if logDir != "." && logDir != "" {
+			if err := os.MkdirAll(logDir, 0755); err != nil {
+				panic(fmt.Errorf("failed to create log directory: %w", err))
+			}
+		}
+	}
+
 	writers := []io.Writer{
 		os.Stdout,
 		&lumberjack.Logger{
